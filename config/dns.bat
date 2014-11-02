6f876bd5591d3633672fd00aee7b918b11a5a69f
jfo <jfojfo@gmail.com>
2014-11-02 13:14:34 +0800

    add dns setting

--- config/dns.bat
+++ config/dns.bat
@@ -1,6 +1,11 @@
 D:
 chdir D:\work\project\shadow\shadowsocks
-rem start /MIN C:\Python27\python dnsrelay.py -c config/dns.cfg || pause
+@rem start /MIN /wait C:\Python27\python dnsrelay.py -c config/dns.cfg || pause
+
+netsh interface ipv4 set dns name="无线网络连接" source=static addr=127.0.0.1 register=PRIMARY
 
 D:\work\cygwin64\bin\python2.7.exe dnsrelay.py
+
+netsh interface ipv4 set dns name="无线网络连接" source=dhcp
+
 pause
