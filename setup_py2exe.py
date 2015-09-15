78246a63f1212bf465baca25ce38a7b4032847b4
jfo <jfojfo@gmail.com>
2015-09-15 16:32:42 +0800

    rm __file__, or else local.exe will not run add setup_py2exe.py add libeay32.dll

---
+++ setup_py2exe.py
@@ -0,0 +1,47 @@
+import sys
+from os.path import join, dirname
+from distutils.core import setup
+import py2exe
+
+sys.path.append(join(dirname(__file__), 'shadowsocks'))
+sys.path.append(join(dirname(__file__), 'shadowsocks', 'crypto'))
+
+
+import codecs
+
+with codecs.open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="shadowsocks",
+    version="2.8.2",
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    description="A fast tunnel proxy that help you get through firewalls",
+    author='clowwindy',
+    author_email='clowwindy42@gmail.com',
+    url='https://github.com/shadowsocks/shadowsocks',
+    packages=['shadowsocks', 'shadowsocks.crypto'],
+    package_data={
+        'shadowsocks': ['README.rst', 'LICENSE']
+    },
+    install_requires=[],
+    console=["shadowsocks/local.py","shadowsocks/server.py"],
+    entry_points="""
+    [console_scripts]
+    sslocal = shadowsocks.local:main
+    ssserver = shadowsocks.server:main
+    """,
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Internet :: Proxy Servers',
+    ],
+    long_description=long_description,
+)
